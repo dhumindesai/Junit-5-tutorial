dbff6b16dfeee05aa9db40b39f76bf16a672103a
dhumindesai <dhrumin.desai28@gmail.com>
2019-05-27 17:02:15 -0400

    implemented TDD methodology

--- src/test/java/MathUtilsTest.java
+++ src/test/java/MathUtilsTest.java
@@ -5,11 +5,17 @@ import static org.junit.jupiter.api.Assertions.*;
 class MathUtilsTest {
 
     @Test
-    void add() {
+    void testAdd() {
         MathUtils mu = new MathUtils();
         int expected = 2;
         int actual = mu.add(1,1);
 
         assertEquals(expected, actual);
+    }
+
+    @Test
+    void testCalculateCircleAria(){
+        MathUtils mu = new MathUtils();
+        assertEquals(314.1592653589793,mu.calculateCircleArea(10), "Must return correct circle area");
     }
 }
